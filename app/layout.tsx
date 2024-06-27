ec0a1eecaa0d863f108edbf897e26f9cf3ea6ed5
Alexandre Guillome <alexandre1906@live.fr>
2024-06-27 15:52:15 +0200

    updates icons v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,6 @@ export const metadata: Metadata = {
     apple: ["/apple-touch-icon.png?v=4"],
     shortcut: ["/apple-touch-icon.png"],
   },
-  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
